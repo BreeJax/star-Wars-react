05afc32c22db2dde0629acf1e40a6cbee06d200d
Bree Jackson <bjacksonsisterwolf@gmail.com>
2018-02-08 16:26:28 -0500

    there's a tutorial that shows that react apollo is the way to go- we'll see how this works

--- src/App.js
+++ src/App.js
@@ -2,11 +2,92 @@ import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 
+const StarWarsTable = () => (
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Gender</th>
+      <th>Homeward Name</th>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+    </tr>
+  </table>
+)
+
+query StarWarsCharacter{
+  person(personID: 4) {
+    name
+    gender
+    homeworld {
+      name
+    }
+  }
+}
+
 class StarWarsApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:62159/")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          })
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <ul>
+          {items.map((item) => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      )
+    }
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="StarWarsApp">
         <Header />
+        <StarWarsTable />
         <Footer />
       </div>
     )
